1f954776044cb196849502d8f59af0d3b6d7fff3
KataokaNagi <calm359@gmail.com>
2021-02-24 03:55:21 +0900

    template: add all, rall, &chmax/min

--- template/src/b.cpp
+++ template/src/b.cpp
@@ -4,8 +4,35 @@ using namespace std;
 #define repm(i, n) for (int i = (int)(n - 1); i >= 0; i--)
 #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
 #define repm2(i, s, n) for (int i = (int)(n - 1); i >= (s); i--)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
 using ll = long long; // more than 10^9
 using P = pair<int, int>;
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b; // aをbで更新
+        return true;
+    }
+    return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b; // aをbで更新
+        return true;
+    }
+    return false;
+}
 
 int main()
 {
