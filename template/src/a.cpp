a7b7c5ca0bca02a00a5fe45c454c6d770167498e
KataokaNagi <calm359@gmail.com>
2021-04-22 20:00:02 +0900

    template: update macro

--- template/src/a.cpp
+++ template/src/a.cpp
@@ -1,35 +1,46 @@
 #include <bits/stdc++.h>
 #include <unordered_set>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define repm(i, n) for (int i = (int)(n - 1); i >= 0; i--)
-#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
-#define repm2(i, s, n) for (int i = (int)(n - 1); i >= (s); i--)
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define reps(i, x) for (int i = 1; i <= (int)(x); ++i)
+#define repi(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define rrep(i, n) for (int i = (int)(n - 1); i >= 0; --i)
+#define rreps(i, x) for (int i = ((int)(x)); i > 0; --i)
+#define rrepi(i, a, b) for (int i = int(b); i >= int(a); --i)
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
+#define sz(x) ((int)(x).size())
+#define fill(x, y) memset(x, y, sizeof(x))
+#define pb push_back
+#define mp make_pair
+#define fore(e, v) for (auto e : v)
+#define forkv(c) for (const auto &[key, value] : c)
 using ll = long long; // more than 10^9
 using P = pair<int, int>;
 const int INF = 0x3fffffff;
-template <typename T>
+template <class T>
 bool chmax(T &a, const T &b)
 {
     if (a < b)
     {
         a = b;
-        return true;
+        return 1;
     }
-    return false;
+    return 0;
 }
-template <typename T>
+template <class T>
 bool chmin(T &a, const T &b)
 {
-    if (a > b)
+    if (b < a)
     {
         a = b;
-        return true;
+        return 1;
     }
-    return false;
+    return 0;
 }
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 
 int main()
 {
